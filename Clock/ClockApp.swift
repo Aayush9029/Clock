d7cd216ea0a667e548356b6ba0815f26e26b4bdb
Aayush9029 <aayushpokharel36@gmail.com>
2021-10-15 18:21:13 -0400

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Aayush Pokharel on 2021-10-15.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
